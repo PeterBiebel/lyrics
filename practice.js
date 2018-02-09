d1a4cf3a2ab5c3188e26bdab004deac9648205da
Peter Biebel <phbiebelatl@gmail.com>
2018-02-09 13:36:34 -0500

    js file

--- practice.js
+++ practice.js
@@ -1,25 +1,20 @@
+$('button').click(function() {
+ 
+    let artist= $('.js-query-artist').val();
+    let title= $('.js-query-title').val();
+     console.log(artist, title)
+	getLryics(artist, title);
+})
 
-$( "button" ).click(function() {
-	
-	let artist= $('.js-query-artist').val()
-	let title= $('.js-query-title').val()
-	console.log(artist, title)
-//  alert( "Handler for .click() called." );
-    findLyrics(artist, title);
-    
-});
-
-
-function findLyrics(artist, title) {
-     console.log(artist, title);
-      $.get(`https://api.lyrics.ovh/v1/${artist}/${title}`, function(data, status){
-      	console.log(data, status)
-      	displayLyrics(data);
-       // alert("Data: " + data + "\nStatus: " + status);
-    });
-  }
+function getLryics(artist, title) {
+	console.log(artist, title);
+	$.get( `https://api.lyrics.ovh/v1/${artist}/${title}`, function( data ) {
+        console.log(data);
+        displayLyrics(data.lyrics);
+		})
+	};
 
 function displayLyrics(data) {
-
-	$('.text').text(data.lyrics);
-}  
+    
+     $('.text').text(data);
+}	
